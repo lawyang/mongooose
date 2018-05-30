5059a300f633820451d80e41b585dcc8dca2871f
Law Yang <lawtouayang@gmail.com>
2018-05-30 14:14:17 -0500

    connected mongoose

--- server/server.js
+++ server/server.js
@@ -5,9 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Require in out Mongoose Model
+const Book = require('./Modules/models/book.schema');
+// Connect to MongoDB
+const mongoose = require('mongoose');
 
+const DATABASE_NAME = 'library'
+const DATABASE_URL = `mongodb://localhost:27017/${DATABASE_NAME}`; 
+mongoose.connect(DATABASE_URL);
 
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose is connected to ${DATABASE_URL}`);
+});
 
+mongoose.connection.on('error', (error) => {
+    console.log(`Mongoose connetion error: ${error}`);
+});
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
 
